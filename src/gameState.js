1e09c44622b940f2f56aebbb5a9776c94aae968d
Soloman Garcia <soloman.garcia@gmail.com>
2020-06-01 12:30:54 -0600

    add to gameState

--- src/gameState.js
+++ src/gameState.js
@@ -1,10 +1,25 @@
 const gameState = {
   current: "INIT",
   clock: 1,
+  wakeTime: -1,
   tick() {
     this.clock++;
-    console.log(this.clock);
+
+    if (this.clock === this.wakeTime) {
+      this.wake();
+    }
+
     return this.clock;
+  },
+  startGame() {
+    console.log("hatching");
+    this.current = "HATCHING";
+    this.wakeTime = this.clock + 3;
+  },
+  wake() {
+    console.log("hatched");
+    this.current = "IDLING";
+    this.wakeTime = -1;
   },
   handleUserAction(icon) {
     console.log(icon);
